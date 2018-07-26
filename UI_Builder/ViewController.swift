1750bb5ad2c93ffd6286be1e3466e188e566c80e
ECEn Student <ecestudent@fb110-ee10349.local>
2018-07-26 09:29:14 -0600

    Initial Commit

---
+++ UI_Builder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Builder
+//
+//  Created by ECEn Student on 7/26/18.
+//  Copyright © 2018 Nicholas Bragg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
